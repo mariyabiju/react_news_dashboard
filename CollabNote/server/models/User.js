f8e27e9a8e40fa82b4aece0a5b7911798facfe94
mariam <119947537+mariyabiju@users.noreply.github.com>
2025-11-19 19:24:38 +0530

    Delete CollabNote directory

--- CollabNote/server/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-const userSchema= new mongoose.Schema({
-    username: String,
-    email: { type: String, unique: true },
-    password: String,
-    sharedKey: { type: String, unique: true }
-
-})
-module.exports = mongoose.model("User",userSchema)
